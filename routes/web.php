b57543db8c042bd848a192bfc07e2aea5cf2f11b
safi khan <safifalak@gmail.com>
2020-10-05 13:59:13 +0545

    update

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::get('/', function () {
 });
 
 
-Route::get('deploy/{key}','Admin\DeployController@deploy');
+Route::post('deploy/{key}','Admin\DeployController@deploy');
 
 
 // including admin panel routes
