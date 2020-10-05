d4403a52bed9a2b626ef579cdb0dbe55bf6f4ba3
safi khan <safifalak@gmail.com>
2020-10-05 17:32:44 +0545

    update

--- resources/views/frontend/shared/leftbar.blade.php
+++ resources/views/frontend/shared/leftbar.blade.php
@@ -1,8 +1,10 @@
 <div class="card mb-5">
-    <div class="card-header">Most Viewed</div>
+    <div class="card-header">{{__("Most Viewed")}}</div>
     <ul class="list-group list-group-flush">
         @forelse(getMostCount('views_count') as $news)
-            <li class="list-group-item">{{$news->title}}</li>
+            <li class="list-group-item">
+                <a href="{{route('frontend.news.detail', [app()->getLocale(),$news, $news->slug])}}">{{$news->title}}</a>
+            </li>
         @empty
             <li class="list-group-item">No data avaialable</li>
         @endforelse
@@ -13,6 +15,6 @@
 </div> -->
 
 <div class="card mb-5">
-    <div class="card-header">Categories</div>
+    <div class="card-header">{{__("Categories")}}</div>
     <category-component></category-component>
 </div>
--- resources/views/frontend/shared/rightbar.blade.php
+++ resources/views/frontend/shared/rightbar.blade.php
@@ -1,8 +1,12 @@
 <div class="card mb-5">
-    <div class="card-header">Most Commented</div>
+    <div class="card-header">{{__("Most Commented")}}</div>
     <ul class="list-group list-group-flush">
         @forelse(getMostCount('comments_count') as $news)
-            <li class="list-group-item">{{$news->title}}</li>
+            <li class="list-group-item">
+                <a href="{{route('frontend.news.detail', [app()->getLocale(),$news, $news->slug])}}">
+                    {{$news->title}}
+                </a>
+            </li>
         @empty
             <li class="list-group-item">No data avaialable</li>
         @endforelse
@@ -10,10 +14,12 @@
 </div>
 
 <div class="card mb-5">
-    <div class="card-header">Most Liked</div>
+    <div class="card-header">{{__("Most Liked")}}</div>
     <ul class="list-group list-group-flush">
         @forelse(getMostCount('likes_count') as $news)
-            <li class="list-group-item">{{$news->title}}</li>
+            <li class="list-group-item">
+                <a href="{{route('frontend.news.detail', [app()->getLocale(),$news, $news->slug])}}">{{$news->title}}</a>
+            </li>
         @empty
             <li class="list-group-item">No data avaialable</li>
         @endforelse
